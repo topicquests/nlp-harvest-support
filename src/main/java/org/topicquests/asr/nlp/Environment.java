e013373e410aa82d763b825cc21705e7b7048099
Jack Park <jackpark@gmail.com>
2025-06-30 15:12:32 -0700

    More config support

--- src/main/java/org/topicquests/asr/nlp/Environment.java
+++ src/main/java/org/topicquests/asr/nlp/Environment.java
@@ -3,10 +3,13 @@
  */
 package org.topicquests.asr.nlp;
 
+import java.util.Map;
+
 import org.topicquests.asr.nlp.api.IProcessModel;
 import org.topicquests.asr.nlp.model.ProcessModel;
 import org.topicquests.asr.nlp.parsers.PMCPullParser;
 import org.topicquests.asr.nlp.parsers.PubMedReportPullParser;
+import org.topicquests.asr.nlp.config.ConfigPullParser;
 
 /**
  * 
@@ -15,11 +18,17 @@ public class Environment {
 	private PubMedReportPullParser pubmedPP;
 	private PMCPullParser pmcPP;
 	private IProcessModel model;
+	private Map<String,Object>properties;
+	private final String configPath = "config/props.xml";
 	/**
 	 * 
 	 */
 	public Environment() {
 		//TODO logging setup
+		// config
+		ConfigPullParser p = new ConfigPullParser(configPath);
+		properties = p.getProperties();
+
 		pubmedPP = new PubMedReportPullParser(this);
 		pmcPP = new PMCPullParser(this);
 		model = new ProcessModel(this);
@@ -37,6 +46,20 @@ public class Environment {
 		return model;
 	}
 	
+	public Map<String, Object> getProperties() {
+		return properties;
+	}
+	
+	public String getStringProperty(String key) {
+		return (String)properties.get(key);
+	}
+	//////////////////
+	// Config
+	/////////////////
+	public Object getProperty(String key) {
+		return properties.get(key);
+	}
+
 	////////////////////
 	// Logging
 	// TODO
---
+++ src/main/java/org/topicquests/asr/nlp/config/ConfigurationHelper.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 TopicQuests
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.topicquests.asr.nlp.config;
+
+import java.io.File;
+
+/**
+ * 
+ * @author jackpark
+ *
+ */
+public class ConfigurationHelper {
+	public ConfigurationHelper() {
+	}
+
+	public static String findPath(String inFilePath) {
+		File f = new File(inFilePath);
+		if (f.exists()) {
+			return inFilePath;
+		} else {
+			String result = "config/" + inFilePath;
+			f = new File(result);
+			if (f.exists()) {
+				return result;
+			} else {
+				result = "conf/" + inFilePath;
+				f = new File(result);
+				if (f.exists()) {
+					return result;
+				} else {
+					result = "cfg/" + inFilePath;
+					f = new File(result);
+					if (f.exists()) {
+						return result;
+					} else {
+						throw new RuntimeException("File " + result + " not found");
+					}
+				}
+			}
+		}
+	}
+}
