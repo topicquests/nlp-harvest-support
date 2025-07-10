51888b8bc414e91983f63e0ec735558d15fb6f5b
Jack Park <jackpark@gmail.com>
2025-07-10 14:12:45 -0700

    abstract support

---
+++ src/main/java/org/topicquests/asr/nlp/doc/AibstractPojo.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package org.topicquests.asr.nlp.doc;
+
+import org.topicquests.asr.nlp.api.IAbstract;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * 
+ */
+public class AibstractPojo implements IAbstract {
+	private final String
+		PARAGRAPHS		= "paras",
+		SECTIONS		= "sects",
+		TITLE			= "titl",
+		TEXT			= "txt",
+		LANG			= "lang";
+	
+	private JsonObject data;
+	/**
+	 * 
+	 */
+	public AibstractPojo() {
+		data = new JsonObject();
+	}
+	public AibstractPojo(JsonObject data) {
+		this.data = data;
+	}
+	
+	@Override
+	public void addSection(String title, String text, String language) {
+		JsonArray ja = listSections();
+		JsonObject jo = new JsonObject();
+		jo.addProperty(TITLE, title);
+		jo.addProperty(TEXT, text);
+		jo.addProperty(LANG, language);
+		ja.add(jo);
+
+	}
+
+	@Override
+	public void addParagraph(String text, String language) {
+		JsonArray ja = listParagraphs();
+		JsonObject jo = new JsonObject();
+		jo.addProperty(TEXT, text);
+		jo.addProperty(LANG, language);
+		ja.add(jo);
+	}
+
+	@Override
+	public JsonObject getData() {
+		return data;
+	}
+
+	/**
+	 * 
+	 * @return does not return ){@code null}
+	 */
+	JsonArray listParagraphs() {
+		JsonArray ja = data.get(SECTIONS).getAsJsonArray();
+		if (ja == null) {
+			ja = new JsonArray();
+			data.add(SECTIONS, ja);
+		}
+		return ja;
+	}
+	
+	/**
+	 * 
+	 * @return does not return ){@code null}
+	 */
+	JsonArray listSections() {
+		JsonArray ja = data.get(PARAGRAPHS).getAsJsonArray();
+		if (ja == null) {
+			ja = new JsonArray();
+			data.add(PARAGRAPHS, ja);
+		}
+		return ja;
+	}
+}
